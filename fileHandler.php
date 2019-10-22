6762ef533eb10c60dc217d1347418c5ea669639c
gjuniorj <gjuniorj@gmail.com>
2019-10-22 10:49:40 -0300

    Creating XML handler.

--- fileHandler.php
+++ fileHandler.php
@@ -1,19 +1,18 @@
 <?php
 
 
-$something_to_search = ".pdf";
+$something_to_search = ".xml";
 
-search_file('../../../teste',$something_to_search);
+search_file('../../../teste',$something_to_search, 'getXMLAttribute');
 //search_file('.',$file_to_search);
 
 
 /**
- * Find file inside specified directory
- *
  * @param string $dir
- * @param string $file_to_search
+ * @param string $something_to_search
+ * @param $callback
  */
-function search_file($dir,$file_to_search){
+function search_file($dir,$something_to_search, $callback){
 
     //Returns an array with directories and files names found inside given directory
     $files = scandir($dir);
@@ -28,11 +27,11 @@ function search_file($dir,$file_to_search){
         if(!is_dir($path)) {
 
             //It's a file. Searches for a file with specified extension.
-            if(strcmp($file_to_search, getFileExtension($value)) == 0){
+            if(strcmp($something_to_search, getFileExtension($value)) == 0){
 
-                //var_dump($file_to_search); var_dump(substr($value,-3));
-                echo "file found".PHP_EOL;
-                echo $path.PHP_EOL;
+//                echo 'arquivo => '.$value;
+//                echo 'Vai chamar callback';
+                $callback($path);
 
             }
 
@@ -40,7 +39,7 @@ function search_file($dir,$file_to_search){
         elseif($value != "." && $value != "..") {
 
             //Searches file inside found directory
-            search_file($path, $file_to_search);
+            search_file($path, $something_to_search, $callback);
 
         }
     }
@@ -49,18 +48,62 @@ function search_file($dir,$file_to_search){
 function getFileExtension($haystack){
 
     $needlePos = strrpos($haystack,'.');
+
+    //Verifies if file has an extension
     if ( $needlePos === false ){
         //echo "ERROR - File name must have an extension.";
-        var_dump($haystack);
-        //exit();
+        //var_dump($haystack);
+        return '';
     }
 
     $fileExtension =  substr($haystack, $needlePos);
 
-   // var_dump($fileExtension);
+   return $fileExtension;
 
-    return $fileExtension;
+}
+
+/**
+ * @param $xmlFilePath
+ * @return int|SimpleXMLElement|string
+ */
+function getXMLAttribute($xmlFilePath){
+
+    //Loading xml file to object
+    $xml = simplexml_load_file($xmlFilePath, null, null);
+
+    $typesArray = array();
+
+    //Iterating table attributes
+    for ($i=0; $i < count($xml->table); $i++){
+
+        //verifies if field tag exists
+        if ( !is_null($xml->table[$i]->field) ){
+
+            //Iterating field tags of table
+            for ($j=0; $j < count($xml->table[$i]->field); $j++){
+
+                //Gets type attribute from field tag/node
+                $typeAttribute = $xml->table[$i]->field[$j]->attributes()['type'];
+
+                if ( !is_null($typeAttribute) ) {
+                    array_push($typesArray, $typeAttribute);
+                }
+            }
 
 
+        }
+    }
+    var_dump($typesArray);
 
+//    //Iterating attributes of field tag in the xml file
+//    foreach($xml as $attribute => $value) {
+//
+//        //Verifies if attribute name is "type"
+//        if ( strcmp($attribute, 'type') === 0){
+//            //echo $attribute.' => '.$value;
+//        }
+//
+//    }
+
+    //return $attribute;
 }
